fa11c632eef61c93c7c0ff67534ca98b8ad60796
asheshwor <asheshwor@gmail.com>
2016-03-16 03:24:10 +1030

    :octocat: added code for error handling

--- getdevices.R
+++ getdevices.R
@@ -4,10 +4,15 @@ dev.url <- "http://192.168.0.1/DEV_device.htm" #this one is for netgear router; 
 user.name <- "adminUserName"
 user.password <- "adminPassword"
 dev.html <- GET(dev.url, authenticate(user.name, user.password)) #get the html
-dev.html <- htmlParse(dev.html)
+dev.html <- htmlParse(dev.html) #see handling errors below
 dev.html.table <- getNodeSet(dev.html, "//table")[[2]] #access devices table
 dev.table <- readHTMLTable(dev.html.table, skip.rows = 1,
                      header = c("sno","ip","name", "mac"),
                      colClasses = c("numeric","character","character", "character"),
                      trim = TRUE, stringsAsFactors = FALSE)
 dev.table #display table on console
+
+
+#handling errors
+if (http_error(dev.html)) print(http_status(dev.url)) else
+  dev.html <- htmlParse(dev.html)
